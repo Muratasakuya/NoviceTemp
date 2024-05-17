8c45109b3d26f764d5d379655dbc49c88e3141f6
MurataSakuya <144077681+Muratasakuya@users.noreply.github.com>
2024-05-17 14:41:09 +0900

    opencvの追加

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	Novice::Initialize(kWindowTitle, 1280, 720);
 
 	// キー入力結果を受け取る箱
-	char keys[256] = {0};
-	char preKeys[256] = {0};
+	char keys[256] = { 0 };
+	char preKeys[256] = { 0 };
 
 	// ウィンドウの×ボタンが押されるまでループ
 	while (Novice::ProcessMessage() == 0) {
